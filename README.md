dae41678549136de2754e1ed1abacc03c6920e29
Bradley Newman <bnewman227@gmail.com>
2020-04-17 22:01:12 +0930

    Improve script to use relative paths for execution, copy terminal icon to AppData, allow script to be run on machines with Windows installed on drive letters other than C

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ A PowerShell script that creates an option in the file explorer context menu to 
 ![Context Menu](https://i.imgur.com/rAN6VUel.png)
 
 ## Installation
-1. Clone this repo to the root (C:\\>)
+1. Clone this repo
 2. Set Execution Policy for CurrentUser to RemoteSigned by running
    PowerShell as Admin and running `Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser` 
 
 3. Run the install.ps1 script as Admin from PowerShell
-`C:\WinTermContext> .\install-uninstall.ps1` 
+`.\install-uninstall.ps1`
 
 > To uninstall run the the script again
--- install-uninstall.ps1
+++ install-uninstall.ps1
@@ -1,6 +1,7 @@
 $x = $env:USERPROFILE
 $x -match "C:\\Users\\(?<content>.*)"
 $user = $matches['content']
+$installdir = "$env:localappdata\\WinTerminalContextMenu"
 
 # Store the registry key location in $location
 $location = 'Registry::HKEY_CLASSES_ROOT\Directory\Background\shell'
@@ -14,21 +15,28 @@ if (Test-Path "$location\WindowsTerminal") {
     Write-Verbose 'Uninstalling' -Verbose
     Remove-Item -Path "$location\WindowsTerminal" -Recurse
     Remove-Item -Path "$location\WindowsTerminalAdmin" -Recurse
+    Remove-Item -Path $installDir -Recurse
 }
 else {
+
+    # Copy terminal icon to AppData
+
+    New-Item  $installdir -ItemType Directory
+    Copy-Item "$PSScriptRoot\\Icon\\terminal.ico" -Destination $installdir
 
     # If not create new key called WindowsTerminal
     New-Item -Path "$location" -Name 'WindowsTerminal' -Value "Open Terminal here"
     # Add the icon path property
-    New-ItemProperty -Path "$location\WindowsTerminal" -Name "Icon" -Value "C:\\WinTermContext\\Icon\\terminal.ico" -PropertyType 'String'
+    New-ItemProperty -Path "$location\WindowsTerminal" -Name "Icon" -Value "$installDir\\terminal.ico" -PropertyType 'String'
 
     # Create new key called command in WindowsTerminal
-    New-Item -Path "$location\WindowsTerminal" -Name 'command' -Value "C:\\Users\\$user\\AppData\\Local\\Microsoft\\WindowsApps\\wt.exe -d ."
+    New-Item -Path "$location\WindowsTerminal" -Name 'command' -Value "$env:localappdata\\Microsoft\\WindowsApps\\wt.exe -d ."
 
     # Create new key called WindowsTerminalAdmin
     New-Item -Path "$location" -Name 'WindowsTerminalAdmin' -Value "Open Terminal as Admin here"
     # Add the icon path property
-    New-ItemProperty -Path "$location\WindowsTerminalAdmin" -Name "Icon" -Value "C:\\WinTermContext\\Icon\\terminal.ico" -PropertyType 'String'
+    
+    New-ItemProperty -Path "$location\WindowsTerminalAdmin" -Name "Icon" -Value "$installDir\\terminal.ico" -PropertyType 'String'
 
     # Create new key called command in WindowsTerminalAdmin
     New-Item -Path "$location\WindowsTerminalAdmin" -Name 'command' -Value "Powershell -WindowStyle Hidden -Command `"Start-Process wt.exe -ArgumentList '-d .' -Verb runAs`"" -ItemType 'String'
@@ -36,3 +44,5 @@ else {
     Pop-Location
 
 }
+
+Pause
